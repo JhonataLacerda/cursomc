f00bdd5fa13fceae9bb44f96b294b4cbb010baa3
Jhonata Lacerda <jonata_5000@hotmail.com>
2020-01-29 03:36:04 -0300

    Projeto criado

---
+++ src/main/java/com/teste/cursommc/CurcommcApplication.java
@@ -0,0 +1,13 @@
+package com.teste.cursommc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurcommcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurcommcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/cursommc/CurcommcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.cursommc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurcommcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
