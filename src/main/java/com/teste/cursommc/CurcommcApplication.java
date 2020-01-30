3534bc27136ec7fa59fca78913451e6d34ae0e1b
Jhonata Lacerda <jonata_5000@hotmail.com>
2020-01-30 03:27:03 -0300

    Operacao de instanciacao dos produtos

--- src/main/java/com/teste/cursommc/CurcommcApplication.java
+++ src/main/java/com/teste/cursommc/CurcommcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.teste.cursommc.domain.Categoria;
+import com.teste.cursommc.domain.Produto;
 import com.teste.cursommc.repositories.CategoriaRepository;
+import com.teste.cursommc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CurcommcApplication implements CommandLineRunner{
 	
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	@Autowired
+	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CurcommcApplication.class, args);
@@ -25,7 +29,17 @@ public class CurcommcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		Categoria cat3 = new Categoria(null,"Ferramentas");
+		Produto p1 = new Produto(null,"Mouse",30.90);
+		Produto p2 = new Produto(null,"Impressora", 100.00);
+		Produto p3 = new Produto(null,"Computador",900.00);
+		cat1.setProdutos(Arrays.asList(p1,p2,p3));
+		cat2.setProdutos(Arrays.asList(p2));
+		p1.setCategorias(Arrays.asList(cat1));
+		p2.setCategorias(Arrays.asList(cat1,cat2));
+		p3.setCategorias(Arrays.asList(cat1));
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 		
 	}
 }
